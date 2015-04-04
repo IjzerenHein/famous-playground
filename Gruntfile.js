23736ba0cacee57aac91ae4e4eac863567c4e741
IjzerenHein <hrutjes@gmail.com>
2015-04-04 16:19:28 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		eslint: {
+			target: ['app/src/*.js', 'app/src/**/*.js'],
+			options: {
+				config: '.eslintrc'
+			}
+		},
+		jscs: {
+				src: ['app/src/*.js', 'app/src/**/*.js'],
+				options: {
+					config: '.jscsrc'
+				}
+		},
+		lesslint: {
+				src: ['app/styles/styles.less'],
+				options: {
+					imports: ['**/*.less'],
+					csslint: {
+						csslintrc: '.csslintrc'
+					}
+				}
+		},
+		'ftp-deploy': {
+      build: {
+        auth: {
+          host: 'ftp.pcextreme.nl',
+          port: 21,
+          authKey: 'gloey.nl'
+        },
+        src: 'dist',
+        dest: '/domains/gloey.nl/htdocs/www/apps/playground'
+      }
+    },
+		exec: {
+			'build': 'webpack -d',
+			'build-prod': 'webpack -p',
+			'open': 'open www/index.html',
+			'clean': 'rm -rf ./www',
+			'serve': 'webpack-dev-server -d --inline --reload=localhost',
+			'open-serve': 'open http://localhost:8080'
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-lesslint');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+
+	// Default task.
+	grunt.registerTask('default', ['eslint', 'jscs', 'lesslint', 'exec:build']);
+	grunt.registerTask('prod', ['exec:clean', 'eslint', 'jscs', 'lesslint', 'exec:build-prod']);
+	grunt.registerTask('open', ['exec:open']);
+	grunt.registerTask('clean', ['exec:clean']);
+	grunt.registerTask('serve', ['exec:open-serve', 'exec:serve']);
+	grunt.registerTask('open-serve', ['exec:open-serve']);
+	grunt.registerTask('deploy', ['eslint', 'jscs', 'lesslint', 'exec:build', 'ftp-deploy']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# famous-playground
-General purpose playground repo for famo.us
+#famous-playground
+
+General purpose playground repo for famo.us.
+
+
+# Index
+- [Installation](#installation)
+- [Building & running](#building--running)
+- [Resources](#resources)
+
+
+# Installation
+
+First make sure you have node.js installed... without that nothing works!
+You can either install it with your favorite package manager or with [the installer](http://nodejs.org/download) found on [nodejs.org](http://nodejs.org).
+
+Install grunt & webpack globally *(if not installed already):*
+
+```
+npm install -g grunt-cli webpack webpack-dev-server
+```
+
+Install the local npm packages *(from the project root folder):*
+
+```
+npm install
+```
+
+That's it! You should now be able to build & run the project.
+
+
+# Building & running
+
+To build the project, use:
+
+```
+grunt
+```
+
+This runs linters & code-style checkers, and builds the webpack output in: `/www`.
+You can open the output `www/index.html` file using `grunt open`.
+
+To start a live reload server, use:
+
+```
+grunt serve
+```
+
+This starts the webpack-dev-server at `http://localhost:8080` and opens the URL in the browser.
+
+To build for production and minify and mangle the output, use:
+
+```
+grunt prod
+```
+
+
+# Resources
+
+- [Webpack How-to](https://github.com/petehunt/webpack-howto)
+
+
+
+## Contribute
+
+If you like this project and want to support it, show some love
+and give it a star.
+
+
+## Contact
+- 	@IjzerenHein
+- 	http://www.gloey.nl
+- 	hrutjes@gmail.com (for hire)
+
+© 2015 - Hein Rutjes
